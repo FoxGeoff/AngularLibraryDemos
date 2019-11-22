21c9e78aae6e6727f94c96097e8e90756a353d2b
FoxGeoff <fox.geoffrey@gmail.com>
2019-11-21 16:24:20 -0800

    Add: JavaScript Module

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var print_module_1 = require("./print_module");
+var prontModule = new print_module_1.PrintModule();
+prontModule.printMessage('Hello from Libray');
---
+++ main.ts
@@ -0,0 +1,4 @@
+import {PrintModule} from './print_module';
+
+const prontModule = new PrintModule();
+prontModule.printMessage('Hello from Libray');
